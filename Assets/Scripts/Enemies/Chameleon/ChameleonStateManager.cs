091e72bd651557ff4b2328f36c6ec554ab86df1c
huytl2107 <huytl2107@gmail.com>
2024-01-19 00:37:15 +0700

    fix nốt con chameleon

--- Assets/Scripts/Enemies/Chameleon/ChameleonStateManager.cs
+++ Assets/Scripts/Enemies/Chameleon/ChameleonStateManager.cs
@@ -20,4 +20,24 @@ public class ChameleonStateManager : EnemiesStateManager
         HandleGroundDetection();
         PlayerCheck();
     }
+
+    public override void HandleGroundDetection()
+    {
+        if (SeeGround || !GroundAheadCheck.IsOverlappingGround)
+        {
+            //Vì Sprite của con này lệch qua 1 bên nên cần cập nhật lại ví trí cho nó hiển thị chuẩn
+            Rb.transform.position = new Vector2(Rb.transform.position.x - 2.5f * RaycastDirX, Rb.transform.position.y);
+            FlipXObject();
+        }
+    }
+
+    //Func này sẽ được gọi trong 1 farme của Animation Attack;
+    private void PlayerGotHit()
+    {
+        if (SeePlayer)
+        {
+            Player.CurrentState = Player.State.GotHit();
+            Player.CurrentState.EnterState();
+        }
+    }
 }
