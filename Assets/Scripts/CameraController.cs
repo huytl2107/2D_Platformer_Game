ec3f3fb796e1d597fd54066244573891674d63b2
huytl2107 <huytl2107@gmail.com>
2024-01-22 23:43:07 +0700

    offset and clamp camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mono.CompilerServices.SymbolWriter;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private Vector3 _offset;
     [SerializeField] private Transform player;
-    [SerializeField] private float _plusY;
+    [SerializeField] private float _offSightXLeft;
+    [SerializeField] private float _offSightXRight;
 
     void FixedUpdate()
     {
-        transform.position = new Vector3(player.position.x, player.position.y + _plusY, transform.position.z);
+        if (player.transform.position.x < _offSightXLeft)
+        {
+            transform.position = new Vector3(_offSightXLeft, player.position.y + _offset.y, transform.position.z + _offset.z);
+        }
+        else if (player.transform.position.x > _offSightXRight)
+        {
+            transform.position = new Vector3(_offSightXRight, player.position.y + _offset.y, transform.position.z + _offset.z);
+        }
+        else
+        {
+            transform.position = new Vector3(player.position.x + _offset.x, player.position.y + _offset.y, transform.position.z + _offset.z);
+        }
     }
 }
